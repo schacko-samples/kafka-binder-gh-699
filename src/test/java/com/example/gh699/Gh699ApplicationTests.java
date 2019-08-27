161b925a1b6669aa13d87b39f502e3ba77d7a9d7
Soby Chacko <schacko@pivotal.io>
2019-08-27 15:54:02 -0400

    Initial commit

---
+++ src/test/java/com/example/gh699/Gh699ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gh699;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gh699ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
