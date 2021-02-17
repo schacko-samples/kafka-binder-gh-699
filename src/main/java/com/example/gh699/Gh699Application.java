cf0420dda00ad60c6573389e1b3f8413413595aa
Soby Chacko <schacko@pivotal.io>
2021-02-17 15:50:00 -0500

    New updates

--- src/main/java/com/example/gh699/Gh699Application.java
+++ src/main/java/com/example/gh699/Gh699Application.java
@@ -4,12 +4,11 @@ import java.util.function.Function;
 import java.util.logging.Logger;
 
 import brave.kafka.streams.KafkaStreamsTracing;
-
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Transformer;
-import org.apache.kafka.streams.kstream.TransformerSupplier;
 import org.apache.kafka.streams.processor.ProcessorContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -23,40 +22,34 @@ public class Gh699Application {
 		SpringApplication.run(Gh699Application.class, args);
 	}
 
-
 	@Bean
-	public Function<KStream<String, String>, KStream<String, String>> foo(TransformerSupplier<String, String, KeyValue<String, String>> transformerSupplier) {
-		return input -> input.transform(transformerSupplier);
+	public Function<KStream<String, String>, KStream<String, String>> foo(KafkaStreamsTracing kafkaStreamsTracing) {
+		return input -> input.transform(kafkaStreamsTracing.transformer(
+				"transformer-1",
+				() -> new Transformer<String, String, KeyValue<String, String>>() {
+					ProcessorContext context;
+
+					@Override
+					public void init(ProcessorContext context) {
+						this.context = context;
+					}
+
+					@Override
+					public KeyValue<String, String> transform(String key, String value) {
+						try {
+							Thread.sleep(100L);
+						}
+						catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						LOG.info("Headers: " + this.context.headers());
+						LOG.info("K/V:" + key + "/" + value);
+						return KeyValue.pair(key, value);
+					}
+
+					@Override
+					public void close() {
+					}
+				}));
 	}
-
-	@Bean
-	public TransformerSupplier<String, String, KeyValue<String, String>> bar(KafkaStreamsTracing kafkaStreamsTracing) {
-				return kafkaStreamsTracing.transformer(
-						"transformer-1",
-						new Transformer<String, String, KeyValue<String, String>>() {
-							ProcessorContext context;
-
-							@Override
-							public void init(ProcessorContext context) {
-								this.context = context;
-							}
-
-							@Override
-							public KeyValue<String, String> transform(String key, String value) {
-								try {
-									Thread.sleep(100L);
-								} catch (InterruptedException e) {
-									e.printStackTrace();
-								}
-								LOG.info("Headers: " + this.context.headers());
-								LOG.info("K/V:" + key + "/" + value);
-								return KeyValue.pair(key, value);
-							}
-
-							@Override
-							public void close() {
-							}
-						});
-	}
-
 }
